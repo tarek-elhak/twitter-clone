a568e271228e10c04471401c7ee76ee5e7b24393
tarek abdelhak mohamed <tarekroja123@gmail.com>
2020-05-06 23:46:53 +0200

    setup for basic bages

---
+++ twitter-clone-client/src/pages/home.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+class home extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <h1>home page</h1>
+      </div>
+    );
+  }
+}
+
+export default home;
---
+++ twitter-clone-client/src/pages/signup.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+class signup extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <h1>signup page</h1>
+      </div>
+    );
+  }
+}
+export default signup;
